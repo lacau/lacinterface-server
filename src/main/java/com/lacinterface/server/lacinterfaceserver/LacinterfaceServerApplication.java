21e099a0e155365c806e9a5d6784816f6911bcc5
lacau <lacau@zpay.com.br>
2019-06-03 16:34:09 -0300

    Initial commit.

---
+++ src/main/java/com/lacinterface/server/lacinterfaceserver/LacinterfaceServerApplication.java
@@ -0,0 +1,13 @@
+package com.lacinterface.server.lacinterfaceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LacinterfaceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LacinterfaceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lacinterface/server/lacinterfaceserver/LacinterfaceServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lacinterface.server.lacinterfaceserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LacinterfaceServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
